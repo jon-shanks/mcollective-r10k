e36e5f56c48c63ff9748be1338b909dd38d03c04
Jonathan Shanks <jon.shanks@gmail.com>
2014-03-25 17:35:34 +0000

    adding in r10k.rb

--- usr/libexec/mcollective/mcollective/agent/r10k.rb
+++ usr/libexec/mcollective/mcollective/agent/r10k.rb
@@ -4,13 +4,35 @@ module MCollective
 
       activate_when do
         File.exists?('/usr/bin/r10k')
+        File.exists?('/usr/bin/sudo')
         File.executable?('/usr/bin/r10k')
-        File.directory?('/etc/puppet/environments')
+        File.directory?('/etc/puppet')
       end
 
       action "deploy" do
         r10k = '/usr/bin/r10k'
-        if File.exists?(r10k) and File.executable?(r10k)
+        sudo = '/usr/bin/sudo'
+        datadir = '/etc/puppet'
+        user    = get_user
+        conf    = get_config
+        env     = get_env
+        pfile   = get_pfile
+        sudo_command = "#{sudo} -u #{user} #{r10k} deploy environment"
+        if env
+          sudo_command += " env"
+        end
+        if conf
+          sudo_command += " --config #{conf}"
+        end
+        if pfile
+          sudo_command += " -p"
+        end
+
+        begin
+          Log.debug("#{sudo_command}")
+          reply[:status] = run("#{sudo_command}", :stdout => :out, :stderr => :err, :chomp => true, :cwd => datadir )
+        rescue => e
+          reply.fail(reply[:status] = "Error running sudo command: %s" % e.to_s)
         end
       end
 
@@ -19,7 +41,7 @@ module MCollective
       end
 
       def get_config
-        request[:config] || '/etc/r10k.yaml'
+        request[:config] || false
       end
 
       def get_env
@@ -28,28 +50,6 @@ module MCollective
 
       def get_pfile
         request[:puppet_file] || false
-      end
-
-      def deploy
-        user    = get_user
-        conf    = get_config
-        env     = get_env
-        pfile   = get_pfile
-        datadir = '/etc/puppet'
-        sudo_command = "sudo -u #{user} #{r10k} deploy environment"
-        if env
-          sudo_command += " env"
-        end
-        if conf
-          sudo_command += " -c #{conf}"
-        end
-        if pfile
-          sudo_command += " -p"
-        end
-
-        reply[:status] = run("sudo #{sudo_command}", :stdout => :out, :stderr => :err, :chomp => true, :cwd => datadir )
-        reply[:out]
-        reply[:err]
       end
 
     end
